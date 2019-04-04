fc4673190ae743c3cfc962ba101dc16cbb935037
Kirill Bogatikoff <kivi15penza@gmail.com>
2019-04-05 02:51:40 +0300

    Animation for splashes was refactored, optimized and moved to index.js

--- js/index.js
+++ js/index.js
@@ -48,3 +48,23 @@ $(window).bind("onrolechange", function() {
 	clearTimeout(CONTROLS_UPDATER);
 	updateControls();
 });
+
+const SPLASH_ANIMATION_TIME = 1500;
+const SPLASH_TIMEOUT = 8500;
+
+var currentSplash = 0;
+function showNextSplash() {
+	if(++currentSplash > 5) {
+		currentSplash = 1;
+    };
+	
+	$("#splash-1").attr("src", "/img/splash/" + currentSplash + ".png");
+    $("#splash-2").fadeOut(SPLASH_ANIMATION_TIME, function() {
+        $("#splash-2").attr("src", "/img/splash/" + currentSplash + ".png").css("display", "flex");
+        setTimeout(showNextSplash, SPLASH_TIMEOUT);
+    });
+}
+
+$(document).ready(function() {
+    showNextSplash();
+});
