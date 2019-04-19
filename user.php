40670f5dbc697c54be6d8a253b3636b880a46c9e
Kirill Bogatikoff <kivi15penza@gmail.com>
2019-04-19 21:38:21 +0300

    Добавлена страница личного кабинета пользователя и иконка ошибки

---
+++ user.php
@@ -0,0 +1,24 @@
+<!doctype>
+<html>
+<?php
+
+
+
+?>
+	<head>
+	   <meta charset="UTF-8">
+	   <title>Insert title here</title>
+	   <meta name="description" content="Сервис для создания дипломов, грамот, открыток, благодарственных писем">
+       <meta name="keywords" content="Дипломы, грамоты, письма, открытки, maker, diploma, charter, letter">
+       <link rel="stylesheet" href="css/common.css"/>
+       <link rel="stylesheet" href="css/user.css"/>
+       <link rel="shortcut icon" href="img/favicon.ico"/>
+       <style id="fonts"></style>
+    </head>
+	<body>
+		<div id="no-access">
+			<img src="/img/error.png"/>
+			<h1>У Вас нет доступа к этой информации</h1>
+		</div>
+	</body>
+</html>
